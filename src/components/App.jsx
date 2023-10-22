fadd2239aa8e8501f4dcd41aee720b049f61aa9d
nikitkachan <nikyc777@gmail.com>
2023-10-22 18:26:24 +0300

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -13,16 +13,7 @@ padding: 30px;
 export class App extends Component {
   
  state = {
-   contacts: [
-    {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-     { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    {id: 'id-5', name: 'Rosieq Simpson', number: '459-12-56'},
-    {id: 'id-6', name: 'Hermioqne Kline', number: '443-89-12'},
-    {id: 'id-7', name: 'Edqen Clements', number: '645-17-79'},
-    {id: 'id-8', name: 'Annqie Copeland', number: '227-91-26'},
-  ],
+   contacts: [],
   filter: ''
 }
   
